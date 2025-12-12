b541c971d75e503d06ec8bdff723bb5704c7c7cd
Amanderson Rodrigo <171895801+amandersonrodrigo@users.noreply.github.com>
2025-12-12 02:59:43 -0300

    aula de variaveis concluida

---
+++ Javascript/variaveis/script.js
@@ -0,0 +1,12 @@
+let nome = "Amanderson Rodrigo";
+let idade = 28;
+let comida;
+comida = "Lasanha"
+
+let esposa,
+    trabalho,
+    irmao1,
+    pai,
+    mae;
+
+console.log(nome, idade, comida, esposa, trabalho, irmao1, pai, mae);
