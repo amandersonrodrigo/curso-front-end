e135a0eebcf49761729de73a5ec170e089182802
Amanderson Rodrigo <171895801+amandersonrodrigo@users.noreply.github.com>
2025-12-19 03:33:26 -0300

    atualizando

---
+++ Javascript/numeros-e-operadores/script.js
@@ -0,0 +1,13 @@
+var total = 10 + 5 * 2 / 2 + 20;
+console.log(total);
+
+var aleatorio = "20s";
+var soma1 = aleatorio * 2;
+var soma2 = aleatorio / 3;
+console.log(soma1, soma2);
+
+var s = +'200' + 50;
+console.log(s);
+
+var x = 5;
+console.log(++x);
