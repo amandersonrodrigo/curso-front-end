b096a52ea0d90aedbd89d431403ef62b935e6ad8
Amanderson Rodrigo <171895801+amandersonrodrigo@users.noreply.github.com>
2025-12-30 03:23:56 -0300

    atualizando exercicio

--- dom-para-iniciantes/selecionar-o-dom/script-ex.js
+++ dom-para-iniciantes/selecionar-o-dom/script-ex.js
@@ -1,19 +1,19 @@
 // Retorne no console todas as imagens do site
-const todasImagens = document.querySelectorAll('img');
-console.log(todasImagens);
-
-// Retorne no console apenas as imagens que começaram com a palavra imagem
-const img = document.querySelectorAll('[src^="img/imagem"]');
+const img = document.querySelectorAll('img');
 console.log(img);
 
+// Retorne no console apenas as imagens que começaram com a palavra imagem
+const imagens = document.querySelectorAll('[src^="img/imagem"]');
+console.log(imagens);
+
 // Selecione todos os links internos (onde o href começa com #)
-const linksInterno = document.querySelectorAll('[href^="#"]');
-console.log(linksInterno);
+const linksInternos = document.querySelectorAll('[href^="#"]');
+console.log(linksInternos);
 
 // Selecione o primeiro h2 dentro de .animais-descricao
-const primeroH2 = document.querySelector('.animais-descricao h2');
-console.log(primeroH2);
+const primeiroH2 = document.querySelector('.animais-descricao h2');
+console.log(primeiroH2);
 
 // Selecione o último p do site
-const paragrafos = document.querySelectorAll('p');
-console.log(paragrafos[paragrafos.length - 1]);
+const ultimoP = document.querySelectorAll('p');
+console.log(ultimoP[ultimoP.length - 1]);
