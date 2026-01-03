1d961094c8f53da70fe102349802b5b11a924963
Amanderson Rodrigo <171895801+amandersonrodrigo@users.noreply.github.com>
2026-01-03 03:32:11 -0300

    atualizando aulas

--- dom-para-iniciantes/foreach-e-arrow-function/script.js
+++ dom-para-iniciantes/foreach-e-arrow-function/script.js
@@ -1,6 +1,23 @@
+// const imgs = document.querySelectorAll('img');
+
+// let i = 0;
+// imgs.forEach(function(item, index, array){
+//    // console.log(item, index, array);
+// });
+
+const titulos = document.getElementsByClassName('titulo');
+const titulosArray = Array.from(titulos);
+
+console.log(titulos);
+console.log(titulosArray);
+
+titulosArray.forEach(function(item, index, array){
+   // console.log(item, index, array);
+});
+
 const imgs = document.querySelectorAll('img');
 
 let i = 0;
-imgs.forEach(function(item, index, array){
-   // console.log(item, index, array);
+imgs.forEach((item, index, array) => {
+   console.log(item, index, array);
 });
