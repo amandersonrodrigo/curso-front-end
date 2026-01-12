7a542c61ae27406fa4e71207eaba1e5f322949f8
Amanderson Rodrigo <171895801+amandersonrodrigo@users.noreply.github.com>
2026-01-12 12:20:05 -0300

    atualizado

--- dom-para-iniciantes/classes-e-atriutos/script-ex.js
+++ dom-para-iniciantes/classes-e-atriutos/script-ex.js
@@ -1,28 +1,26 @@
 // Adicione a classe ativo a todos os itens do menu
-const menu = document.querySelectorAll('.menu a');
-menu.forEach((item) => {
-   item.classList.add('ativo');
+
+const ativo = document.querySelectorAll('.menu a');
+ativo.forEach((item) => {
+   item.classList.add('ativo')
 });
 
 // Remove a classe ativo de todos os itens do menu e mantenha apenas no primeiro
 
-menu.forEach((item) => {
-   item.classList.remove('ativo');
+ativo.forEach((item) => {
+   item.classList.remove('ativo')
 });
-
-menu[0].classList.add('ativo');
+ativo[0].classList.add('ativo');
 
 // Verifique se as imagens possuem o atributo alt
-const imgs = document.querySelectorAll('img');
 
-imgs.forEach((img) => {
-   const possuiAlt = img.hasAttribute('alt');
-   console.log(img, possuiAlt);
+const imgs = document.querySelectorAll('img');
+imgs.forEach((item) => {
+   const possuiAlt = item.hasAttribute('alt')
+   console.log(item, possuiAlt);
 });
 
 // Modifique o href do link externo no menu
+
 const linkExterno = document.querySelector('a[href^="http"]');
-
-linkExterno.setAttribute('href', 'https://www.google.com/');
-
-console.log(linkExterno);
+linkExterno.setAttribute('href', 'https://www.google.com');
--- dom-para-iniciantes/foreach-e-arrow-function/script-ex.js
+++ dom-para-iniciantes/foreach-e-arrow-function/script-ex.js
@@ -1,12 +1,14 @@
 // Mostre no console cada parágrado do site
-const paragrafos = document.querySelectorAll('p');
-console.log(paragrafos);
-
-paragrafos.forEach(item => console.log(item));
+const paragrafo = document.querySelectorAll('p');
+// console.log(paragrafo);
+paragrafo.forEach((item) => {
+  console.log(item)
+});
 
 // Mostre o texto dos parágrafos no console
-paragrafos.forEach(item => console.log(item.innerText));
-
+paragrafo.forEach((item) => {
+  console.log(item.innerText);
+});
 // Como corrigir os erros abaixo:
 const imgs = document.querySelectorAll('img');
 
@@ -21,4 +23,3 @@ imgs.forEach(() => {
 
 imgs.forEach(() => i++);
 
-console.log(i);
