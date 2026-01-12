e45e08d68b21fa5c316ce370c96b51d988afea15
Amanderson Rodrigo <171895801+amandersonrodrigo@users.noreply.github.com>
2026-01-12 09:38:14 -0300

    atualizando

---
+++ dom-para-iniciantes/eventos/script.js
@@ -0,0 +1,58 @@
+const img = document.querySelector('img');
+
+function callback(event) {
+   console.log(event);
+}
+
+// img.addEventListener('click', callback);
+
+const animaisLista = document.querySelector('.animais-lista');
+
+function callbackLista(e) {
+   console.log(e.currentTarget);
+   console.log(e.target);
+   console.log(e.type);
+}
+
+// animaisLista.addEventListener('click', callbackLista);
+
+const linkExterno = document.querySelector('a[href^="http"]');
+
+function handleLinkExterno(event) {
+   event.preventDefault();
+   // console.log(event);
+   console.log(this);
+}
+
+linkExterno.addEventListener('click', handleLinkExterno);
+
+const h1 = document.querySelector('h1');
+
+function handleEvent(event) {
+   console.log(event.type, event); 
+}
+
+// h1.addEventListener('click', handleEvent);
+// h1.addEventListener('mouseenter', handleEvent);
+// h1.addEventListener('mousemove', handleEvent);
+
+// window.addEventListener('scroll', handleEvent);
+// window.addEventListener('resize', handleEvent);
+
+function hadleKeyboard(event) {
+   if(event.key === 'a') {
+      document.body.classList.toggle('azul');
+   }
+}
+
+window.addEventListener('keydown', hadleKeyboard);
+
+const imgs = document.querySelectorAll('img');
+
+function handleImg(evento) {
+   console.log(evento.target);
+}
+
+imgs.forEach((img) => {
+   img.addEventListener('click', handleImg);
+})
--- dom-para-iniciantes/selecionar-o-dom/script-ex.js
+++ dom-para-iniciantes/selecionar-o-dom/script-ex.js
@@ -1,19 +1,20 @@
 // Retorne no console todas as imagens do site
-const img = document.querySelectorAll('img');
-console.log(img);
+const imgs = document.querySelectorAll('img');
+console.log(imgs);
 
 // Retorne no console apenas as imagens que começaram com a palavra imagem
-const imagens = document.querySelectorAll('[src^="img/imagem"]');
-console.log(imagens);
+const img = document.querySelectorAll('[src^="img/imagem"]');
+console.log(img);
 
 // Selecione todos os links internos (onde o href começa com #)
-const linksInternos = document.querySelectorAll('[href^="#"]');
+const linksInternos = document.querySelectorAll('a[href^="#"]');
 console.log(linksInternos);
 
 // Selecione o primeiro h2 dentro de .animais-descricao
-const primeiroH2 = document.querySelector('.animais-descricao h2');
-console.log(primeiroH2);
+const h2 = document.querySelector('.animais-descricao h2');
+console.log(h2);
 
 // Selecione o último p do site
-const ultimoP = document.querySelectorAll('p');
-console.log(ultimoP[ultimoP.length - 1]);
+const paragrafo = document.querySelectorAll('p');
+
+console.log(paragrafo[paragrafo.length - 1]);
