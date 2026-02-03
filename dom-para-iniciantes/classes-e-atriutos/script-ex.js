c22179e48ccb881aa87c8e1b7b4c110103d858c6
Amanderson Rodrigo <171895801+amandersonrodrigo@users.noreply.github.com>
2026-02-03 04:01:04 -0300

    Update script-ex.js

--- dom-para-iniciantes/classes-e-atriutos/script-ex.js
+++ dom-para-iniciantes/classes-e-atriutos/script-ex.js
@@ -1,27 +1,26 @@
 // Adicione a classe ativo a todos os itens do menu
+const menu = document.querySelectorAll('.menu a');
 
-const ativo = document.querySelectorAll('.menu a');
-ativo.forEach((item) => {
-   item.classList.add('ativo')
+menu.forEach((item) => {
+   item.classList.add('ativo');
 });
+
 
 // Remove a classe ativo de todos os itens do menu e mantenha apenas no primeiro
-
-ativo.forEach((item) => {
-   item.classList.remove('ativo')
+menu.forEach((item) => {
+   item.classList.remove('ativo');
 });
-ativo[0].classList.add('ativo');
+
+menu[0].classList.add('ativo');
 
 // Verifique se as imagens possuem o atributo alt
-
 const imgs = document.querySelectorAll('img');
-imgs.forEach((item) => {
-   const possuiAlt = item.hasAttribute('alt')
-   console.log(item, possuiAlt);
+
+imgs.forEach((img) => {
+   console.log(img.hasAttribute('alt'));
 });
 
 // Modifique o href do link externo no menu
 
-const linkExterno = document.querySelector('a[href^="http"]');
-linkExterno.setAttribute('href', 'https://www.google.com');
-console.log(linkExterno);
+const linkExterno = document.querySelector('a[href^="https"]');
+linkExterno.setAttribute('href', 'https://www.linkedin.com/in/amanderson-rodrigo-45a980226/');
