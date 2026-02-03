a9a42ab449a24a9ec6f4dede2624775866b5ebbe
Amanderson Rodrigo <171895801+amandersonrodrigo@users.noreply.github.com>
2026-02-03 03:33:42 -0300

    Update script-ex.js

--- dom-para-iniciantes/foreach-e-arrow-function/script-ex.js
+++ dom-para-iniciantes/foreach-e-arrow-function/script-ex.js
@@ -1,25 +1,27 @@
-// Mostre no console cada parágrado do site
-const paragrafo = document.querySelectorAll('p');
-// console.log(paragrafo);
-paragrafo.forEach((item) => {
-  console.log(item)
+// Mostre no console cada parágrafo do site
+const paragrafos = document.querySelectorAll('p');
+// console.log(paragrafos);
+
+paragrafos.forEach((paragrafo) => {
+  console.log(paragrafo);
 });
 
+
 // Mostre o texto dos parágrafos no console
-paragrafo.forEach((item) => {
-  console.log(item.innerText);
+paragrafos.forEach((paragrafo) => {
+  console.log(paragrafo.innerText);
 });
+
 // Como corrigir os erros abaixo:
 const imgs = document.querySelectorAll('img');
 
 imgs.forEach((item, index) => {
-  console.log(item, index);
+  // console.log(item, index);
 });
 
 let i = 0;
 imgs.forEach(() => {
-  console.log(i++);
+  // console.log(i++);
 });
 
 imgs.forEach(() => i++);
-
