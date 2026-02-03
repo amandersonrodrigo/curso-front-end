fde1311a0ade887f98c0c8b0e38071f11f447b98
Amanderson Rodrigo <171895801+amandersonrodrigo@users.noreply.github.com>
2026-02-03 12:51:54 -0300

    atualizado

--- dom-para-iniciantes/classes-e-atriutos/script-ex.js
+++ dom-para-iniciantes/classes-e-atriutos/script-ex.js
@@ -1,26 +1,26 @@
 // Adicione a classe ativo a todos os itens do menu
-const menu = document.querySelectorAll('.menu a');
+const links = document.querySelectorAll('.menu a');
 
-menu.forEach((item) => {
-   item.classList.add('ativo');
+links.forEach((link) => {
+   link.classList.add('ativo');
 });
-
 
 // Remove a classe ativo de todos os itens do menu e mantenha apenas no primeiro
-menu.forEach((item) => {
-   item.classList.remove('ativo');
+links.forEach((link) => {
+   link.classList.remove('ativo');
 });
 
-menu[0].classList.add('ativo');
+links[0].classList.add('ativo');
 
 // Verifique se as imagens possuem o atributo alt
 const imgs = document.querySelectorAll('img');
 
 imgs.forEach((img) => {
-   console.log(img.hasAttribute('alt'));
+   const possuiAlt = img.hasAttribute('alt');
+   console.log(possuiAlt, img);
 });
 
 // Modifique o href do link externo no menu
-
 const linkExterno = document.querySelector('a[href^="https"]');
 linkExterno.setAttribute('href', 'https://www.linkedin.com/in/amanderson-rodrigo-45a980226/');
+
