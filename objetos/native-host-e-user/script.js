6aa7d01e628255913d48e382adcc7202b683b0df
Amanderson Rodrigo <171895801+amandersonrodrigo@users.noreply.github.com>
2026-02-26 02:48:41 -0300

    atualizando

---
+++ objetos/native-host-e-user/script.js
@@ -0,0 +1,18 @@
+// Construtores de objetos nativos
+// Object
+// String
+// Array
+// Function
+
+// Objetos do Browser 
+// NodeList
+// HTMLCollection
+// Element
+
+console.log(typeof Array.fromasd !== "undefined")
+
+if(typeof Array.from === "undefined") {
+   console.log('Não existe!');
+} else {
+   console.log('Existe essa propriedade/método');
+}
---
+++ objetos/native-host-e-user/scriptEx.js
@@ -0,0 +1,23 @@
+// Liste 5 objetos nativos
+Object
+String
+Array
+Function
+Number
+
+// Liste 5 objetos do browser
+Window
+Document
+history
+NodeList
+HTMLCollection
+
+
+// Liste 2 Métodos, Propriedades ou Objetos
+// presentes no Chrome que não existem no Firefox
+
+if (window.webkitRequestFileSystem === 'undefined') {
+   console.log('Não existe!!');
+} else {
+   console.log('Existe!!');
+}
